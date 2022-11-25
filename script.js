4bc183d40f30241cf858077bdec8b5fefcd91c93
dausahayy <118981031+dausahayy@users.noreply.github.com>
2022-11-25 12:24:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function myfunction() {
+    
+    document.getElementById("pop"). innerHTML= alert ("tampar kah") ;
+    document.getElementById("pop"). innerHTML= alert ("kd measi lalu") ;
+    document.getElementById("pop"). innerHTML= alert ("di padahi tu pantar di suruh") ;
+
+    document.getElementById("judul"). innerHTML ="bilang liwar nah, kalu ku tampar";
+
+    document.getElementById("gambar").src = "man.png";
+
+    document.getElementById("tulisan"). innerHTML = "kalu di padahi tu pantar di suruh";
+
+    document.getElementById("button"). innerHTML = "00000000";
+}
+
+
